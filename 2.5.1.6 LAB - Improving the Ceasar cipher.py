cb39f3b8148b454d5e4747d6189211ce8f236485
Giorgi Beridze <gioberidze@gmail.com>
2022-06-27 23:58:36 +0400

    Improved Caeser Cipher Code

---
+++ 2.5.1.6 LAB - Improving the Ceasar cipher.py
@@ -0,0 +1,40 @@
+# Improving the Caesar cipher
+"""
+our task is to write a program which:
+--> asks the user for one line of text to encrypt;
+--> asks the user for a shift value (an integer number from the range 1..25 - note:
+you should force the user to enter a valid shift value
+(don't give up and don't let bad data fool you!)
+--> prints out the encoded text.
+
+Test data:
+-> Sample input:
+abcxyzABCxyz 123
+2
+-> Sample output:
+cdezabCDEzab 123
+"""
+
+cipher = input('Enter your cryptogram: ')
+while True:
+    try:
+        shift = int(input('choose shift from 1 to 25: '))
+        if 1 < shift < 26:
+            break
+    except ValueError:
+        print("Enter only numbers!")
+text = ''
+for char in cipher:
+    if not char.isalpha():  # check if character is non-alphabetical remain untouched
+        text += char
+        continue
+    code = ord(char) + shift  # letter shift
+    if 65 <= ord(char) <= 90:  # check letter if it is capital
+        if code > 90:  # check if shifted letter is not out of capital letters range
+            code -= 26
+    elif 97 <= ord(char) <= 122:  # check letter if it is lower-case
+        if code > 122:  # check if shifted letter is not out of lower-case letters range
+            code -= 26
+    text += chr(code)
+print(text)
+
